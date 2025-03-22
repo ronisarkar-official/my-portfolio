8e9dc4edb69e966116fd1d015778b977acd82371
Roni Sarkar <ronisarkar742202@gmail.com>
2025-03-22 10:50:10 +0530

    Update Clients.jsx

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import { clientReviews } from '../constants';
 
 const Clients = () => {
@@ -7,25 +9,32 @@ const Clients = () => {
       <h3 className="head-text">Here from My Clients</h3>
       <div className="client-container">
         {clientReviews.map(({ id, name, review, img, position }) => (
-          <div key={id} className="client-review">
+          <motion.div
+            key={id}
+            className="client-review"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+          >
             <div>
               <p className="text-white font-light">{review}</p>
               <div className="client-content">
                 <div className="flex gap-3">
                   <img src={img} alt={name} className="w-12 h-12 rounded-full" />
                   <div className="flex flex-col">
-                    <p className="font-semibold text-white-800">{name}</p>{' '}
+                    <p className="font-semibold text-white-800">{name}</p>
                     <p className="text-white-500 md:text-base text-sm font-light">{position}</p>
                   </div>
                 </div>
                 <div className="flex self-end items-center gap-2">
-                    {Array.from({ length: 5 }).map((_, index) => (
-                        <img key={index} src="assets/star.png" alt="star" className="w-5 h-5" />
-                    ))}
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <img key={index} src="assets/star.png" alt="star" className="w-5 h-5" />
+                  ))}
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </section>
