65ddc5668f74eca512f58e4f2ef907f925fb16ca
Roni Sarkar <ronisarkar742202@gmail.com>
2025-04-21 23:05:13 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -43,7 +43,7 @@ const About = memo(() => {
 
         <div className="col-span-1 xl:row-span-3">
           <div className="grid-container">
-            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+            <img src="assets/grid2.webp" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
