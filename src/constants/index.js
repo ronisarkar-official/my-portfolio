b89f129913aa2bd2eecdb2349955a516c03a8dfa
Roni Sarkar <ronisarkar742202@gmail.com>
2025-04-30 22:10:57 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -85,7 +85,7 @@ export const myProjects = [
       {
         id: 3,
         name: 'JavaScript',
-        path: '/assets/Js.png',
+        path: '/assets/js.png',
       },
       {
         id: 4,
