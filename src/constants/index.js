950bc7ad94aaece0a752fb79d96344bdd1d888e3
Roni Sarkar <ronisarkar742202@gmail.com>
2025-04-30 22:13:30 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -62,7 +62,7 @@ export const myProjects = [
     desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
     subdesc:
       'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: '#',
+    href: 'https://mcet.free.nf',
     texture: '/textures/project/project1.mp4',
     logo: '/assets/project-logo1.png',
     logoStyle: {
