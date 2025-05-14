f4719a8b6643befada64bc3d53f4d354dc85700c
Roni Sarkar <ronisarkar742202@gmail.com>
2025-05-14 09:20:12 +0530

    lag fix

--- src/sections/LogoSection.jsx
+++ src/sections/LogoSection.jsx
@@ -1,25 +1,19 @@
 import { logoIconsList } from '../constants/index.js';
 
-const LogoIcon = ({ icon }) => {
-  return (
-    <div className="flex-none flex-center marquee-item">
-      <img src={icon.imgPath} alt={icon.name} />
-    </div>
-  );
-};
-
 const LogoSection = () => {
+  // Combine the list once to avoid repeated mapping
+  const repeatedIcons = [...logoIconsList, ...logoIconsList];
+
   return (
     <div className="md:my-20 my-10 relative">
       <div className="gradient-edge" />
       <div className="gradient-edge" />
       <div className="marquee h-52">
         <div className="marquee-box md:gap-12 gap-5">
-          {logoIconsList.map((icon) => (
-            <LogoIcon key={icon.name} icon={icon} />
-          ))}
-          {logoIconsList.map((icon) => (
-            <LogoIcon key={icon.name + "-duplicate"} icon={icon} />
+          {repeatedIcons.map((icon, index) => (
+            <div key={`${icon.name}-${index}`} className="flex-none flex-center marquee-item">
+              <img src={icon.imgPath} alt={icon.name} />
+            </div>
           ))}
         </div>
       </div>
